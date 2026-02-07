006fe45fd1a06ee6678a71ba2117195af656186f
kodlabjuniorofficial <kodlabjuniorofficial@gmail.com>
2026-02-07 10:39:42 +0300

    Initialize variables and print user information
    Added variables for name, age, and city, and printed them.

---
+++ etkinlik_4.py
@@ -0,0 +1,19 @@
+isim = "Dede" # isminizi tutan bir değişken
+yas = 95 # yasınızı tutan bir değişken
+sehir = "Ağrı" # şehrinizi tutan bir değişken
+
+# print() ile değişkenleri ekrana yazdıralım. 
+print(isim) 
+print(yas)
+print(sehir)
+
+"""
+Görev:
+Sizde kablumbağaya dedeye kendinizi tanıtmak için
+adınızı, yaşınızı ve en sevdiğiniz rengi değişkenlerde saklayın.
+Sonrasında bunları print() ile ekranda çıktılayın.
+"""
+
+
+
+
