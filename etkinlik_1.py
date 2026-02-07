f4959b09cb1d22ce068c35171a62a7dac1e15e7d
kodlabjuniorofficial <kodlabjuniorofficial@gmail.com>
2026-02-07 10:36:36 +0300

    Initialize turtle graphics and draw a triangle
    Add turtle graphics setup and triangle drawing instructions.

---
+++ etkinlik_1.py
@@ -0,0 +1,26 @@
+import  # kütüphaneyi dahil ediyoruz
+ 
+t = turtle.Turtle() # kütüphaneden kablumbağa kitabını alıyoruz. 
+t.shape("turtle") # şekil seçmek ve göstermek için kullanılır
+
+t.    # kaplumbağa 100 piksel ileri gider
+t.    # kaplumbağa 100 piksel ileri gider
+t.    # kaplumbapa 90 derece sola döner
+t.    # kaplumbağa 90 derece sağa döner
+
+
+
+##------------ 
+t.up()
+t.goto(50,50)
+t.down()
+## -----------
+
+## ÜÇGEN ÇİZİMİ: Görev
+t.forward(100)
+t.left(120)
+## Üçgeni tamamlayın
+
+
+
+
