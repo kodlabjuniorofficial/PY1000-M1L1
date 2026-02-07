776cb955b7c5cab1be5c7ec164a9e6a0f1c7beee
kodlabjuniorofficial <kodlabjuniorofficial@gmail.com>
2026-02-07 13:14:00 +0300

    Create 5.py

---
+++ 5.py
@@ -0,0 +1,13 @@
+"""
+Bir print() ifadesi yazılmış ve sanırım bu sihirli kapıyı açan özel bir cümle! 
+Adınızı yaşınızı ve favori renginizi söylemeniz gerekiyor. 
+Fakat sanırım bu ifade de kullanılan değişkenler oluşturulmamış. Siz tamamlayabilir misiniz?
+"""
+# isim değişkeni
+
+# yas değişkeni
+
+# renk değişkeni
+
+
+print("Benim adım: ",isim,"Yaşım:",yas," En sevdiğim renk: ",renk,"Açıl Susam Açıl!")
