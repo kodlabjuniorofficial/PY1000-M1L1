f7b1353d0ba805b06e1bdca4c1c762d7820cc07c
kodlabjuniorofficial <kodlabjuniorofficial@gmail.com>
2026-02-07 10:38:02 +0300

    Implement basic turtle movements
    Add initial turtle graphics steps in etkinlik_3.py

---
+++ etkinlik_3.py
@@ -0,0 +1,10 @@
+# Bu noktada kütüphaneyi içeri aktarın ve kaplumbağayı canlandırın! 
+
+# 1. Basamak
+t.forward(40)  # İleri (Yatay)
+t.right(90)    # Sağa Dön 90
+t.forward(40)  # İleri (Dikey)
+t.left(90)     # Sola Dön 90 (Yeni basamağa hazırlan)
+# 2. Basamak
+
+# 3. Basamak
