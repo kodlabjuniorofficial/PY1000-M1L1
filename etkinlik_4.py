cbf94b8c0a3da1c40882fda452c0643366e047ad
kodlabjuniorofficial <kodlabjuniorofficial@gmail.com>
2026-02-07 10:41:06 +0300

    Modify comment for print statements
    Updated print statement comment to include 'AÇIKLA'.

--- etkinlik_4.py
+++ etkinlik_4.py
@@ -2,7 +2,7 @@ isim = "Dede" # isminizi tutan bir değişken
 yas = 95 # yasınızı tutan bir değişken
 sehir = "Ağrı" # şehrinizi tutan bir değişken
 
-# print() ile değişkenleri ekrana yazdıralım. 
+# print() ile değişkenleri ekrana yazdıralım. PRİNT("AÇIKLA")
 print(isim) 
 print(yas)
 print(sehir)
